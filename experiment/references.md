ffde308b86617961ac5e56bcf1af1f214401dcc1
Vaibhav Kurhe <vrk.vlab@coeptech.ac.in>
2025-01-22 17:15:41 +0530

    Initial commit

--- experiment/references.md
+++ experiment/references.md
@@ -1,1 +1,34 @@
-### Link your references in here
+## References
+
+-	Omega Engineering. (2019). The Pressure and Vacuum Handbook. Vol. 28. Stamford, CT: Omega Engineering. ISBN 978-1-934391-28-8.
+-	Instrumentation, Systems, and Automation Society (ISA). (2018). Pressure and Vacuum Measurement: A Technician's Guide. 3rd ed. Research Triangle Park, NC: ISA. ISBN 978-1-60582-443-7.
+-	Hughes, T. A. (2017). Measurement and Instrumentation: Theory and Application. 2nd ed. Boca Raton, FL: CRC Press. ISBN 978-1-4987-4391-9.
+
+#### Journal Articles:
+
+-	Lee, S., & Kim, J. (2020). Design and Development of a Calibration Facility for Pressure and Vacuum Sensors. Journal of Sensors and Sensor Systems, 9(2), 1-12. DOI: 10.5194/jsss-9-1-2020.
+-	Patel, S., & Patel, U. (2019). Calibration of Pressure and Vacuum Sensors: A Review. Journal of Instrumentation, 14(2), C02001. DOI: 10.1088/1748-0221/14/02/C02001.
+-	Wang, Y., & Li, Q. (2018). Development of a Calibration System for Pressure and Vacuum Sensors. Journal of Measurement Science and Instrumentation, 9(3), 033301. DOI: 10.1063/1.5023457.
+
+#### Conference Proceedings:
+
+-	International Conference on Instrumentation and Measurement (ICIM 2019). Calibration of Pressure and Vacuum Sensors: Challenges and Opportunities. Proceedings of the 2019 International Conference on Instrumentation and Measurement, Glasgow, UK, August 26-28, 2019.
+-	IEEE Sensors Conference (IEEE SENSORS 2018). Design and Development of a Calibration Facility for Pressure and Vacuum Sensors. Proceedings of the 2018 IEEE Sensors Conference, New Delhi, India, October 28-31, 2018.
+#### Standards and Guidelines:
+
+-	International Organization for Standardization (ISO). (2019). ISO 17025:2019: General Requirements for the Competence of Testing and Calibration Laboratories. Geneva: ISO.
+-	National Institute of Standards and Technology (NIST). (2019). NIST Special Publication 811: Guide for the Use of the International System of Units (SI). Gaithersburg, MD: NIST.
+
+#### Online Resources:
+
+-	Omega Engineering. (n.d.). Pressure and Vacuum Sensors: Calibration and Troubleshooting. Retrieved from https://www.omega.com/pressure-vacuum-sensors-calibration-troubleshooting
+-	Instrumentation, Systems, and Automation Society (ISA). (n.d.). Calibration of Pressure and Vacuum Sensors. Retrieved from https://www.isa.org/standards-and-publications/isa-publications/calibration-of-pressure-and-vacuum-sensors/
+-	
+#### Thesis and Dissertations:
+
+-	Lee, S. (2020). Design and Development of a Calibration Facility for Pressure and Vacuum Sensors. Master's Thesis, Seoul National University, South Korea.
+-	Patel, S. (2019). Calibration of Pressure and Vacuum Sensors: A Review and Development of a Calibration System. Ph.D. Thesis, Indian Institute of Technology, Delhi, India.
+
+
+
+	
---
+++ experiment/simulation/src/popupData.js
@@ -0,0 +1,120 @@
+$("#simDemo").click(function () {
+
+			 $("#modelDialog").removeClass("modal-md");
+			 $("#modelDialog").addClass("modal-xl");
+			htm=''
+				htm=''
+					+'<div class="row" >'
+					+'<div class="container main-container">'
+					+' <h5 class="text-center" style="color: #f8f9fa;background-color: #343a40;padding-bottom:10px;padding-top:10px">PROBLEM STATEMENT</h5>'
+					+'<ul class="nav nav-tabs" id="tabMenu" role="tablist">'
+					+'  <li class="nav-item">'
+					+'    <button class="nav-link active" id="customer-tab1" data-bs-toggle="tab" data-bs-target="#customer1" type="button" role="tab" aria-controls="customer1" aria-selected="true">Customer Requirements</button>'
+					+'  </li>'
+					+'  <li class="nav-item">'
+					+'    <button class="nav-link" id="operation-tab1" data-bs-toggle="tab" data-bs-target="#operation1" type="button" role="tab" aria-controls="operation1" aria-selected="false">Operational Requirements</button>'
+					+'  </li>'
+					+' </ul>'
+					+' <div class="tab-content mt-3" id="tabContent">'
+//					+'   <!-- Customer Requirements -->'
+					+'<div class="tab-pane fade show active" id="customer1" role="tabpanel" aria-labelledby="customer-tab1">'
+//					+'    <h2 class="tab-title">Customer Requirements</h2>'
+					+'<b style="font-size:18px;">Customer Requirements: (Design and development of Pressure and Vacuum sensor calibration facility)</b><br>'
+					+'   <p style="    font-size: 18px;">The customer intends to set-up a Pressure and Vacuum calibration facility which will test '
+					+'flow sensors of following types as minimum requirement. For all the sensors the process connection will be either ½” NPT (F), '
+					+'screwed type OR flange type. The vacuum and pressure range will be from -300 mmWC to 20 Bar.</p>'
+					+'<br>'
+					+'    <ul class="list-group">'
+					+'     <li class="list-group-item">1. Diaphragm type</li>'
+					+'     <li class="list-group-item">2. Strain Gauge type</li>'
+					+'     <li class="list-group-item">3. Piezoelectric sensors</li>'
+					+'     <li class="list-group-item">4. Capacitive sensor</li>'
+					+'     <li class="list-group-item">5. Bourdon tube type sensor</li>'
+					+'     <li class="list-group-item">6. Bellow type sensor and</li>'
+					+'     <li class="list-group-item">7. Provision for any other type of flow sensor which fits in this category</li>'
+					+'   </ul>'
+					+' </div>'
+					
+//					+' <!-- Operational Requirements -->'
+					+'  <div class="tab-pane fade" id="operation1" role="tabpanel" aria-labelledby="operation-tab1">'
+//					+'   <h2 class="tab-title">Operational Requirements</h2>'
+					+'   <ul class="list-group">'
+					+'     <li class="list-group-item">1.	The set-up for pressure and vacuum sensor calibration must be same and selection is to be made before starting the set-up</li>'
+					+'     <li class="list-group-item">2.	Minimum energy is to be used for testing of the sensors</li>'
+					+'     <li class="list-group-item">3.	The entire process of characterizing of sensor must be fully automated</li>'
+					+'     <li class="list-group-item">4.	In case of power failure the set-up should continue from the last reading</li>'
+					+'     <li class="list-group-item">5.	A provision must be made to reduce the pressure and vacuum turbulence</li>'
+					+'     <li class="list-group-item">6.	Multi point characterizing (Calibration) must be provided, the points will be selected by the customer</li>'
+					+'     <li class="list-group-item">7.	The set-up should test three sensors (minimum) at a time</li>'
+					+'     <li class="list-group-item">8.	The duration of the test should not be more than two hours in view of all the characteristics</li>'
+					+'     <li class="list-group-item">9.	For dynamic characteristics a provision should be made as per demand</li>'
+					+'     <li class="list-group-item">10.	The provision is to be made to acquire health of the plant before starting any test</li>'
+					+'     <li class="list-group-item">11.	The entire analysis and report should be made automatically with validation of data, removing outliers, and having audit trail so as to secure the data.</li>'
+					+'     <li class="list-group-item">12.	The test reports need to be stored for minimum one year</li>'
+					+'     <li class="list-group-item">13.	The visualization tool to be deployed for the operators</li>'
+					+'     <li class="list-group-item">14.	Role based reporting structure and dash boards to be implemented using open source software </li>'
+					+'     <li class="list-group-item">15.	The minimum level financial sustainability of the set up is to be ensured and reported as pointers of efficiency</li>'
+					+'     <li class="list-group-item">17.	The alarms and events are to be generated for critical parameters and to be shared based on role.</li>'
+
+
+					+'   </ul>'
+					+'   </div>'
+				+' </div>'
+				    +' </div>'
+					+'</div>'
+			$("#proStrBody").html(htm);
+			
+		});
+			
+$("#procedure2").click(function () {
+	 $("#modelDialog").removeClass("modal-xl");
+	 $("#modelDialog").addClass("modal-md");
+//	$("#modalTitle").html("PROCEDURE");
+	htm=''
+		+' '
+	$("#procedureBody").html(htm);
+});
+			$("#tagDetails").click(function () {
+				 $("#modelDialog").removeClass("modal-xl");
+				 $("#modelDialog").addClass("modal-md");
+//				$("#modalTitle").html("PROCEDURE");
+				var htm=''
+				+'<table class="table table-bordered table-hover" style="font-size:larger;">'
+					+' <thead>'
+					+'<tr class="table-info">'
+					+'<th>Tag</th>'
+					+'<th>Tag Details</th>'
+					+'</tr>'
+					+'</thead>'
+					+' <tbody>'
+					+'<tr>'
+					+'<td>Piezoelectric sensor</td>'
+					+'<td>S1</td>'
+					+'</tr>'
+					+'<tr>'
+					+'<td>Strain Gauge type</td>'
+					+'<td>S2</td>'
+					+'</tr>'
+					+'<tr>'
+					+'<td>Capacitive sensor </td>'
+					+'<td>S3</td>'
+					+'</tr>'
+					+'<tr>'
+					+'<td>Bourdon tube type sensor</td>'
+					+'<td>S4</td>'
+					+'</tr>'
+					+'<tr>'
+					+'<td>Bellow type sensor </td>'
+					+'<td>S5</td>'
+					+'</tr>'
+					+'<tr>'
+					+'<td>Butterfly Valve </td>'
+					+'<td>CV</td>'
+					+'</tr>'
+				
+					+'</tbody>'
+					+'</table>'
+				$("#tagBody").html(htm);
+			});
+				
+	
